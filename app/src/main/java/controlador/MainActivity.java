35c23b6766ea43c4b09ca60018c48b01002a28e7
RafaelaSilvaAp <rafaela.aparecida.silva96@gmail.com>
2016-10-22 21:48:02 -0200

    Criação classe Banco de Dados - com erros ainda

---
+++ app/src/main/java/controlador/MainActivity.java
@@ -0,0 +1,87 @@
+package controlador;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.marcos.myapplication.R;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * Created by Pantufa on 22/10/2016.
+ */
+public class MainActivity extends Activity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void getActivity(View view) {
+        Button bt = (Button) view;
+        Intent intent;
+
+        if (bt.getText().toString().equalsIgnoreCase("Novo Medicamento")) {
+            intent = new Intent(this, NewMedicamentoActivity.class);
+        } else {
+            intent = new Intent(this, ListMedicamentosActivity.class);
+        }
+
+        startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://controlador/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://controlador/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
---
+++ app/src/main/java/controlador/NewMedicamentoActivity.java
@@ -0,0 +1,38 @@
+package controlador;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.marcos.myapplication.R;
+
+/**
+ * Created by Pantufa on 22/10/2016.
+ */
+public class NewMedicamentoActivity extends Activity {
+    private Medicamento medicamento = new Medicamento();
+    private EditText nomeEt;
+    private EditText tipo_dosagemEt;
+    private EditText tempo_tratamentoEt;
+    private EditText doseEt;
+    private EditText intervaloEt;
+    private EditText quantidadeEt;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+
+        nomeEt = (EditText) findViewById(R.id.nome);
+        tipo_dosagemEt = (EditText) findViewById(R.id.tipo_dosagem);
+        tempo_tratamentoEt = (EditText) findViewById(R.id.tempo_tratamento);
+        doseEt = (EditText) findViewById(R.id.dose);
+        intervaloEt = (EditText) findViewById(R.id.intervalo);
+        quantidadeEt = (EditText) findViewById(R.id.quantidade);
+        }
+    }
+
